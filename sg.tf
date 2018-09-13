5aad3eef4dfdab1b441f0064662dae2912024d9f
giuseppeborgese <giuseppeborgese@gmail.com>
2018-09-13 17:53:48 +0200

    security group

---
+++ sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "firstsg" {
+
+    vpc_id      = "vpc-ae8855ca"
+    name        = "Terraform Enterprise test"
+    description = "Terraform Enterprise test"
+
+    tags {
+        Name  = "Terraform Enterprise test"
+    }
+}
+
+/*
+resource "aws_security_group_rule" "httpin" {
+  description       = "HTTP"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.firstsg.id}"
+}
+*/
