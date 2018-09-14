3737083f30a5ad9a82161dca400105b0a1fabe7a
Giuseppe Borgese <giuseppeborgese@gmail.com>
2018-09-14 11:55:51 +0200

    remore rule

--- sg.tf
+++ sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "firstsg" {
         Name  = "Terraform Enterprise test"
     }
 }
-
+/*
 resource "aws_security_group_rule" "httpin" {
   description       = "HTTP"
   type              = "ingress"
@@ -18,3 +18,4 @@ resource "aws_security_group_rule" "httpin" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.firstsg.id}"
 }
+*/
