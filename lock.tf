5eedf3938850fc59009575d13764ba3790f74f92
Giuseppe Borgese <giuseppe.borgese@clavistechnology.com>
2018-09-14 14:51:22 +0200

    added remote state files

--- lock.tf
+++ lock.tf
@@ -2,3 +2,12 @@ provider "aws" {
     region = "us-east-1"
     allowed_account_ids = ["563249796440"]
 }
+
+terraform {
+  backend "s3" {
+    bucket = "test-atlantis.io"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
